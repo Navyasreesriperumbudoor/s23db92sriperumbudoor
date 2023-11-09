40831f2933d1d178c719014ba8587135ff016d9c
NAVYA SREE SRIPERUMBUDOOR <143027942+S567092@users.noreply.github.com>
2023-11-08 20:31:04 -0600

    added files

--- app.js
+++ app.js
@@ -6,11 +6,18 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var birdsRouter = require('./routes/birds');
+var birdRouter = require('./routes/bird');
 var boardRouter = require('./routes/board');
 var chooseRouter = require('./routes/choose');
-
+var resourceRouter = require('./routes/resource');
+var bird = require('./models/bird');
 var app = express();
+//
+require('dotenv').config();
+const connectionString =
+process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,9 +31,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/birds', birdsRouter);
+app.use('/bird', birdRouter);
 app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
+app.use('/resource', resourceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,4 +52,52 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded")});
+
+
+
+
+// We can seed the collection if needed onserver start
+async function recreateDB(){
+ // Delete everything
+ await bird.deleteMany();
+ let instance1 = new bird({bird_name:"sparrow", color:'yellow',speed:15.4});
+ let instance2 = new bird({bird_name:"pigeon", color:'blue',speed:19.4});
+ let instance3 = new bird({bird_name:"peacock", color:'green',speed:17.4});
+
+ instance1.save().then(doc=>{
+ console.log("First object saved")}
+ ).catch(err=>{
+ console.error(err)
+ });
+ instance2.save().then(doc=>{
+  console.log("second object saved")}
+  ).catch(err=>{
+  console.error(err)
+  });
+  instance3.save().then(doc=>{
+    console.log("third object saved")}
+    ).catch(err=>{
+    console.error(err)
+    });
+
+ 
+
+  }
+    let reseed = true;
+if (reseed) {recreateDB();}
+  
+
+
+    
 module.exports = app;
+  
+
+
+
