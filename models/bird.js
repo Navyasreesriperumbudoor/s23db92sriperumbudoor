3b013d5557bb3acac9e4d2b0d8375a10f9468267
NAVYA SREE SRIPERUMBUDOOR <143027942+S567092@users.noreply.github.com>
2023-11-29 19:42:12 -0600

    added file

--- models/bird.js
+++ models/bird.js
@@ -1,8 +1,16 @@
+const { Double } = require("mongodb")
 const mongoose = require("mongoose")
 const birdSchema = mongoose.Schema({
-bird_name: String,
-color: String,
-speed: Number
-})
+    bird_name: {  type: String,
+        minlength:  2, 
+        maxlength:  10,
+        required: true },
+    color: { type: String,
+        required: true },
+    speed:{  type: Number,
+        min: 2,
+        max: 20,
+        required: true }
+    })
 
 module.exports = mongoose.model("bird",birdSchema)
